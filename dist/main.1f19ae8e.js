33ac657f6d091796b897e48b6b5f125766030c46
z-Lw <553870855@qq.com>
2020-12-07 16:40:34 +0800

    nav-2

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -117,13 +117,18 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"epB2":[function(require,module,exports) {
+})({"main.js":[function(require,module,exports) {
 var $siteList = $('.siteList');
 var $lastLi = $siteList.find('li.last');
 var x = localStorage.getItem('数据');
+var bgColor = JSON.parse(localStorage.getItem('color'));
 var xObject = JSON.parse(x);
-var isFalse = true;
+var $body = $('body')[0];
+window.URL = window.URL || window.webkitURL;
+var colorArray = bgColor || ['#fff'];
+var z;
 var logoUrl = '/favicon.ico';
+$body.style.backgroundColor = colorArray[colorArray.length - 1];
 var hashMap = xObject || [{
   logo: '',
   url: 'https://www.acfun.cn',
@@ -227,13 +232,13 @@ var $tips = $('.tips')[0];
 var $cover = $('#cover')[0];
 var $sel = $('.sel');
 var $iconBtn = $('#iconBtn');
-var $color = $('#color')[0];
-var $body = $('body')[0];
 var $dialogText1 = $('.dialogText')[0];
 var $dialogText2 = $('.dialogText')[1];
 
-$color.onchange = function () {
-  $body.style.backgroundColor = $color.value;
+colorElem.onchange = function () {
+  $body.style.backgroundColor = colorElem.value;
+  colorArray.push($body.style.backgroundColor);
+  bgWapper.style.display = 'none';
 };
 
 $input1.onclick = function () {
@@ -297,10 +302,256 @@ $('.siteAdd ').on('click', function () {
     $dialogWapper.style.display = 'none';
   };
 });
+filesWapperBtn.addEventListener('click', function (e) {
+  if (fileElem) {
+    fileElem.click();
+  }
+
+  e.preventDefault();
+}, false);
+colorWapperBtn.addEventListener('click', function (e) {
+  if (colorElem) {
+    colorElem.click();
+  }
+
+  e.preventDefault();
+}, false);
+
+handleFiles = function handleFiles(files) {
+  if (!files.length) {
+    backgroundImage.alt = '无图片';
+  } else {
+    backgroundImage.src = window.URL.createObjectURL(files[0]);
+
+    backgroundImage.onload = function () {
+      window.URL.revokeObjectURL(this.src);
+    };
+
+    bgWapper.style.display = 'none';
+  }
+};
+
+closeBg.onclick = function () {
+  if (bgWapper.style.display !== 'none') {
+    bgWapper.style.display = 'none';
+  }
+};
+
+pen.onclick = function () {
+  if (bgWapper.style.display !== 'block') {
+    bgWapper.style.display = 'block';
+  }
+};
 
 window.onbeforeunload = function () {
   var string = JSON.stringify(hashMap);
   localStorage.setItem('数据', string);
+  var abc = JSON.stringify(colorArray);
+  localStorage.setItem('color', abc);
 };
-},{}]},{},["epB2"], null)
-//# sourceMappingURL=main.07fb971b.js.map
+},{}],"C:/Users/zzLw/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57003" + '/');
+
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      }); // Enable HMR for CSS by default.
+
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
+      }
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID; // html encode message and stack trace
+
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+
+  if (checkedAssets[id]) {
+    return;
+  }
+
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+
+    return true;
+  }
+}
+},{}]},{},["C:/Users/zzLw/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js","main.js"], null)
+//# sourceMappingURL=/main.1f19ae8e.js.map
